1ee9182369facb1fe7cfed045ccffa98f3a13a30
Zach Childers <zachilde@microsoft.com>
2020-09-17 13:14:23 -0700

    adding a debug line jic

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -4,6 +4,7 @@ Param(
 if ([String]::IsNullOrEmpty($TargetName)) {
     $outputDir = "out"
 } else {
+    ls $($Pipeline.Workspace)
     $outputDir = "$($Pipeline.Workspace)/out_$TargetName"
 }
 & "./$outputDir/bin/wp.client"
