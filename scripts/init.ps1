8c2cd231f740fddb8730ff7963ee4f0a48f29ed3
Zach Childers <zachilde@microsoft.com>
2020-09-17 12:14:46 -0700

    build init

---
+++ scripts/init.ps1
@@ -0,0 +1,10 @@
+Param(
+    [Mandatory] $TargetOS
+)
+
+switch ($TargetOS) {
+    "Windows" { choco install ninja -y }
+    "macOS" { brew install ninja }
+    "Linux" { apt install ninja -y }
+    Default {}
+}
