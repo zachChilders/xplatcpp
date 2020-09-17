f5b9e02e7118752fb7dcabec6f8b903e112b7005
Zach Childers <zachilde@microsoft.com>
2020-09-17 12:34:43 -0700

    almost there

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -5,6 +5,6 @@ Param(
 switch ($TargetOS) {
     "Windows" { choco install ninja -y }
     "macOS" { brew install ninja }
-    "Linux" { sudo apt install ninja -y }
+    "Linux" { sudo apt-get install ninja -y }
     Default {}
 }
